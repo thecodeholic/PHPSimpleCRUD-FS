a916df6751630a889172070b668e659a1588a5b9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-03-05 19:39:46 +0400

    Stub methods

--- users/users.php
+++ users/users.php
@@ -2,35 +2,21 @@
 
 function getUsers()
 {
-    return json_decode(file_get_contents(__DIR__ . '/users.json'), true);
+
 }
 
 function getUserById($id)
 {
-    $users = getUsers();
-    foreach ($users as $user) {
-        if ($user['id'] == $id) {
-            return $user;
-        }
-    }
-    return null;
+
 }
 
 
 function createUser($data)
 {
-    $users = getUsers();
-    $users[] = $data;
-    file_put_contents(__DIR__ . '/users.json', json_encode($users));
+
 }
 
 function updateUser($data, $id)
 {
-    $users = getUsers();
-    foreach ($users as $i => $user) {
-        if ($user['id'] == $id) {
-            $users[$i] = $user + $data;
-        }
-    }
-    file_put_contents(__DIR__ . '/users.json', json_encode($users));
+
 }
